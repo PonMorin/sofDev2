62ed918c21752c53e07a238dce2ab3a70770c7f5
Pon <pon.m22@gmail.com>
2022-12-20 21:56:17 +0700

    third commit

--- index.js
+++ index.js
@@ -2,22 +2,24 @@ const express = require('express')
 const app = express()
 // const worker = require('./worker')
 
+app.use(express.static("public"));
+
 app.get('/', (req, res) => {
     res.send('Hello World from Express Framework')
 })
 
-// app.get('/product', (req, res) => {
-//     res.sendFile(__dirname + '/public/index.html')
-// })
+app.get('/product', (req, res) => {
+    res.sendFile(__dirname + '/public/product.html')
+})
 
-// app.get('/about', (req, res) => {
-//     res.send(worker.Hello())
-// })
+app.get('/about', (req, res) => {
+    res.send(worker.Hello())
+})
 
 
-// app.get('*', (req, res) => {
-//     res.send('ไม่พบหน้าที่ร้องขอ (Error 404 Page Not Found)')
-// })
+app.get('*', (req, res) => {
+    res.sendFile(__dirname + '/public/errorPage.html')
+})
 
 app.listen(3000, () =>{
     console.log('start server at port [3000]')
